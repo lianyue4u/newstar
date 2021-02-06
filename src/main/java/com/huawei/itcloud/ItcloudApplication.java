c2c23c0c55d76db19911904b304204a04b1c93e3
conan chan <15920025414@qq.com>
2021-02-06 15:13:05 +0800

    feat(star) new project

---
+++ src/main/java/com/huawei/itcloud/ItcloudApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.itcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItcloudApplication.class, args);
+    }
+
+}
