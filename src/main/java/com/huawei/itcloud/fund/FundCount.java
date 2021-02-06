b8476bb47c3d2f00d20ca20c5a2ebd0c980cca99
conan chan <15920025414@qq.com>
2021-02-06 15:23:30 +0800

    feat(star) second commit

---
+++ src/main/java/com/huawei/itcloud/fund/FundCount.java
@@ -0,0 +1,9 @@
+package com.huawei.itcloud.fund;
+
+public class FundCount {
+
+    public static void main(String[] args) {
+        System.out.println("this is a fund count java class");
+    }
+
+}
